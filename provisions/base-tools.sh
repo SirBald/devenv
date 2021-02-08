55ed77490c24baad11c10c924b47fc77bbacf1c0
SirBald <49649475+SirBald@users.noreply.github.com>
2021-02-08 19:26:06 +0300

    [FEATURE] Install UnZip in Base Tools provision
    [MISC] Minor clean up of the Base Tools provision script

--- provisions/base-tools.sh
+++ provisions/base-tools.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
 
-echo "[SCRIPT] Install base development tools."
+echo '[SCRIPT] Install base development tools.'
 
 apt-get update
 
-echo "[STEP] Install Git."
+echo '[STEP] Install Git.'
 apt-get install --yes git
 
-echo "[STEP] Use 'aspell' spellchecker in 'nano' editor."
+echo '[STEP] Install UnZip.'
+apt-get install --yes unzip
+
+echo '[STEP] Use Aspell spellchecker in Nano editor.'
 apt-get install --yes aspell
 NANO_CONFIG='/etc/nanorc'
 ASPELL_COMMAND='set speller "aspell -x -c"'
 if ! grep -Fxq "${ASPELL_COMMAND}" "${NANO_CONFIG}"; then
-  echo "\n" >> "${NANO_CONFIG}"
-  echo "${ASPELL_COMMAND}" >> "${NANO_CONFIG}"
+  echo '' | tee -a "${NANO_CONFIG}"
+  echo "${ASPELL_COMMAND}" | tee -a "${NANO_CONFIG}"
 fi
 
-echo "[STEP] Bash prompt enchantments."
-BASHRC_FILE='/home/vagrant/.bashrc'
-BASHRC_ENHANCEMENT_FILE='/home/vagrant/.bashrc-enhancements'
+echo '[STEP] Bash prompt enchantments.'
+HOME_DIR='/home/vagrant'
+BASHRC_FILE="${HOME_DIR}/.bashrc"
+BASHRC_ENHANCEMENT_FILE="${HOME_DIR}/.bashrc-enhancements"
 BASHRC_ENHANCEMENT_COMMAND="source ${BASHRC_ENHANCEMENT_FILE}"
 if [ -f "${BASHRC_ENHANCEMENT_FILE}" ]; then
   if ! grep -Fxq "${BASHRC_ENHANCEMENT_COMMAND}" "${BASHRC_FILE}"; then
-    echo "\n" >> "${BASHRC_FILE}"
-    echo "${BASHRC_ENHANCEMENT_COMMAND}" >> "${BASHRC_FILE}"
+    echo '' | tee -a "${BASHRC_FILE}"
+    echo "${BASHRC_ENHANCEMENT_COMMAND}" | tee -a "${BASHRC_FILE}"
   fi
 fi
